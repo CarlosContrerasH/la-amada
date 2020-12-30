50036edaf1798fc2b004bfd7ad30df97cad2710a
carloscontrerashernadez <45575199+carloscontrerashernadez@users.noreply.github.com>
2020-12-30 16:57:39 -0700

    update 2

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,7 +4,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+
  
 
 CMS.registerMediaLibrary(uploadcare)
@@ -12,5 +12,5 @@ CMS.registerMediaLibrary(cloudinary)
 
  
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
