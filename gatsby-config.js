8790a8d30faad3bcd7b0aff66d98ef6b458139f6
carloscontrerashernadez <45575199+carloscontrerashernadez@users.noreply.github.com>
2021-07-01 07:42:47 -0600

    added analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,17 +15,15 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GA-TRACKING_ID", // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          "G-6XB6JYB19F", 
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-      /*  gtagConfig: {
+     gtagConfig: {
           optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
-        },*/
+        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
